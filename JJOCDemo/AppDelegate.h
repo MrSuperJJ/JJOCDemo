2ce4947b8b98b2ad39191304c9258b3188d1506f
叶佳骏 <yejiajun@cmhi.chinamobile.com>
2018-11-29 15:43:54 +0800

    init

---
+++ JJOCDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JJOCDemo
+//
+//  Created by 叶佳骏 on 2018/11/29.
+//  Copyright © 2018 叶佳骏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JJOCDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JJOCDemo
+//
+//  Created by 叶佳骏 on 2018/11/29.
+//  Copyright © 2018 叶佳骏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
